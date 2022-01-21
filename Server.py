59c90cb236401b6dd07fae90ad48f939f819c189
JuicyOneExile <enrique.nucci03@gmail.com>
2022-01-21 11:53:12 +0100

    Update Server.py

--- Server.py
+++ Server.py
@@ -18,11 +18,11 @@ ServerSocket.listen(5)
 
 
 def threaded_client(connection):
-    threaded_client.name = connection.recv(2048) #
+    threaded_client.name = connection.recv(2048) #ricevo il nickname dal client
     
-    file = open(os.path.join('Files',"score.txt"),"r")
-    checklist = file.readlines()
-    while any(str(threaded_client.name.decode('utf-8').upper()) in s for s in checklist) :
+    file = open(os.path.join('Files',"score.txt"),"r") #file aperto in modalita read
+    checklist = file.readlines() #inserisco ogni riga del fine in una list
+    while any(str(threaded_client.name.decode('utf-8').upper()) in s for s in checklist) : #controllo del nickname se già stato inserito
         teprego = "1"
         connection.send(teprego.encode('utf-8'))
         threaded_client.name = connection.recv(2048)
@@ -31,13 +31,13 @@ def threaded_client(connection):
     file.close()
 
     print("\nClient name: " + str(threaded_client.name.decode('utf-8').upper()))
-    while True:
-        whatToDo = 0
-        whatToDo = connection.recv(2048)
-        if whatToDo.decode('utf-8') == "1" :
+    while True: #loop infinito per tenere il server sempre in ascolto
+        whatToDo = 0 #variabile che conterrà la scelta dell'utente nel menù
+        whatToDo = connection.recv(2048) 
+        if whatToDo.decode('utf-8') == "1" : #partita Classificata
             domandList = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144, 150, 156, 162, 168, 174, 180, 186, 192, 198, 204, 210, 216, 222, 228, 234]
             i = 0
-            while i < 10 :
+            while i < 10 : #loop per mandare al client 10 domande e 40 risposte
                     i = i +1
                     with open(os.path.join('Files','PartitaRapida.txt')) as f:
                         mylist = list(f)
@@ -70,7 +70,7 @@ def threaded_client(connection):
             file.write(str(score)+","+name+"\n")
             file.close() 
                         
-        elif whatToDo.decode('utf-8') == "2" :
+        elif whatToDo.decode('utf-8') == "2" : #partita per argomento
             time.sleep(0.01)
             whatToDo2 = connection.recv(2048)
             if whatToDo2.decode('utf-8') == "1" :
@@ -87,7 +87,7 @@ def threaded_client(connection):
                     listaGeneri = list(k)
             domandList = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54]
             i = 0
-            while i < 10 :
+            while i < 10 : #loop per inviare 10 domande e 40 risposte
                     i = i + 1
                     c = random.choice(domandList)
                     domandainvio = listaGeneri[c]
@@ -111,7 +111,7 @@ def threaded_client(connection):
                         time.sleep(0.01)
                         connection.send(str.encode("Falso"))                
 
-        elif whatToDo.decode('utf-8') == "3" :
+        elif whatToDo.decode('utf-8') == "3" : #Classifica
             file = open(os.path.join('Files',"score.txt"),"r")
             readthefile = file.readlines()
             sortedData = sorted(readthefile,reverse=True)
@@ -122,7 +122,7 @@ def threaded_client(connection):
 
 
 
-while True:
+while True: #Creazione di più client
     Client, address = ServerSocket.accept()
     print('Connected to: ' + address[0] + ':' + str(address[1]))
     start_new_thread(threaded_client, (Client, ))
